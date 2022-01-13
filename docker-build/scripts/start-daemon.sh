20d03374b329e97fa2b673b62cae4f62684a4e99
lovehzj <1922919664@qq.com>
2022-01-13 23:14:52 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir $FLINK_HOME/logs
+mkdir -p $FLINK_HOME/logs
 
 java -Xmx128M -Xms128M -XX:MaxDirectMemorySize=256M -jar $FLINK_HOME/mate/flink-mate.jar >>$FLINK_HOME/logs/flink_mate.stdout.log 2>>$FLINK_HOME/logs/flink_mate.stderr.log
